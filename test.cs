2202c8483648d19a04b941c3f41222ddab30e26c
Trenton Morrell <mtrent6@vt.edu>
2019-02-16 12:11:36 -0500

    fine

--- test.cs
+++ test.cs
@@ -1,22 +1,44 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Text;
-using WebHeaderCollection;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
 
+    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
+    }
+
     // Use this for initialization
     void Start()
     {
-
+         ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+        Debug.Log(Get("https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow"));
+        
     }
 
     // Update is called once per frame
@@ -24,34 +46,38 @@ public class Test : MonoBehaviour
     {
 
     }
+   
 
-      private int GetContent()
+    public string Get(string uri)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
         {
-            var content = "";
-
-            var client = new WebClient();
-
-            var headers = new WebHeaderCollection();
-
-            headers.Add(HttpRequestHeader.Accept, "text/html, application/xhtml+xml, */*");
-            //headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
-            headers.Add(HttpRequestHeader.AcceptLanguage, "en-GB");
-            headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko");
-            
-            client.Headers = headers;
-
-            var rawhtml = client.DownloadString("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972");
-
-            HtmlAgilityPack.HtmlDocument Html = new HtmlAgilityPack.HtmlDocument();
-
-            Html.LoadHtml(rawhtml);
-
-            var title = GetTitle(Html);
-
-            var description = GetDescription(Html);
-            Conslole.WriteLine(description);
-
-            return 0;
+            return reader.ReadToEnd();
         }
+    }
+
+    private void GetContent()
+    {
+        string html = string.Empty;
+        string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
+        Debug.Log("!");
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.AutomaticDecompression = DecompressionMethods.GZip;
+        Debug.Log("!!");
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            html = reader.ReadToEnd();
+        }
+        Debug.Log("oof");
+        Debug.Log(html);
+    }
 
 }
