83cb9e67ab081e9408f2e10239ea0d564fa91a7c
Trenton Morrell <mtrent6@vt.edu>
2019-02-16 11:35:11 -0500

    method added

--- test.cs
+++ test.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
-
+using WebHeaderCollection;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -24,5 +24,34 @@ public class Test : MonoBehaviour
     {
 
     }
+
+      private int GetContent()
+        {
+            var content = "";
+
+            var client = new WebClient();
+
+            var headers = new WebHeaderCollection();
+
+            headers.Add(HttpRequestHeader.Accept, "text/html, application/xhtml+xml, */*");
+            //headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            headers.Add(HttpRequestHeader.AcceptLanguage, "en-GB");
+            headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko");
+            
+            client.Headers = headers;
+
+            var rawhtml = client.DownloadString("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972");
+
+            HtmlAgilityPack.HtmlDocument Html = new HtmlAgilityPack.HtmlDocument();
+
+            Html.LoadHtml(rawhtml);
+
+            var title = GetTitle(Html);
+
+            var description = GetDescription(Html);
+            Conslole.WriteLine(description);
+
+            return 0;
+        }
 
 }
